689f162c816f342cef30f1014fd02577934ad790
yuangwang <weiwng@google.com>
2025-12-15 17:19:22 -0500

    upload

--- .apphosting/adapter-server.js
+++ .apphosting/adapter-server.js
@@ -1,3 +1,4 @@
+"use strict";
 var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
@@ -21,50 +22,60 @@ var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__ge
   mod
 ));
 
-// node_modules/wei-nextjs-adapter-test/dist/bin/serve.js
+// ../firebase-framework-tools/packages/@apphosting/adapter-nextjs/dist/bin/serve.js
 var import_http = require("http");
 var import_url = require("url");
 var import_path = __toESM(require("path"), 1);
 var import_fs = __toESM(require("fs"), 1);
 process.env["NODE_ENV"] = "production";
 process.env["NEXT_PRIVATE_MINIMAL_MODE"] = "1";
-console.log("\u{1F680} Starting Native Adapter...");
 async function start() {
   const serverDir = __dirname;
-  try {
-    const configPath = import_path.default.join(serverDir, "firebase-next-config.json");
-    console.log(`\u{1F4E5} Loading config from ${configPath}`);
-    const rawConfig = import_fs.default.readFileSync(configPath, "utf-8");
-    const buildContext = JSON.parse(rawConfig);
-    const nextPath = require.resolve("next/dist/server/next-server", { paths: [serverDir] });
-    const NextServer = require(nextPath).default;
-    const server = new NextServer({
-      dir: serverDir,
-      hostname: "0.0.0.0",
-      port: parseInt(process.env.PORT || "8080"),
-      conf: buildContext.config
-    });
-    console.log("\u23F3 Preparing server...");
-    const requestHandler = server.getRequestHandler();
-    await server.prepare();
-    (0, import_http.createServer)(async (req, res) => {
-      try {
-        const parsedUrl = (0, import_url.parse)(req.url, true);
-        if (!req.headers["x-matched-path"]) {
-          req.headers["x-matched-path"] = parsedUrl.pathname;
+  const nextMetaPath = require.resolve("next/dist/server/request-meta", { paths: [serverDir] });
+  const { NEXT_REQUEST_META } = require(nextMetaPath);
+  const configPath = import_path.default.join(serverDir, "firebase-next-config.json");
+  const rawConfig = import_fs.default.readFileSync(configPath, "utf-8");
+  const buildContext = JSON.parse(rawConfig);
+  const getPostponedState = (path2) => {
+    let prerender = buildContext.outputs.prerenders.find((it) => it.pathname === path2);
+    if (!prerender) {
+      const dynamicMatch = buildContext.routes.dynamicRoutes.find((it) => path2.match(new RegExp(it.sourceRegex)))?.source;
+      prerender = buildContext.outputs.prerenders.find((it) => it.pathname === dynamicMatch);
+    }
+    return prerender?.fallback?.postponedState;
+  };
+  const nextPath = require.resolve("next/dist/server/next-server", { paths: [serverDir] });
+  const NextServer = require(nextPath).default;
+  const server = new NextServer({
+    dir: serverDir,
+    hostname: "0.0.0.0",
+    port: parseInt(process.env.PORT || "8080"),
+    conf: buildContext.config
+  });
+  await server.prepare();
+  const requestHandler = server.getRequestHandler();
+  (0, import_http.createServer)(async (req, res) => {
+    try {
+      const parsedUrl = (0, import_url.parse)(req.url, true);
+      const { pathname } = parsedUrl;
+      if (req.headers["next-resume"] === "1" && pathname) {
+        const postponed = getPostponedState(pathname);
+        if (postponed) {
+          console.log(`\u26A1\uFE0F Injecting Postponed State for ${pathname}`);
+          req[NEXT_REQUEST_META] = {
+            postponed
+          };
         }
-        await requestHandler(req, res, parsedUrl);
-      } catch (err) {
-        console.error("Request Error:", err);
-        res.statusCode = 500;
-        res.end("Internal Error");
       }
-    }).listen(parseInt(process.env.PORT || "8080"), () => {
-      console.log(`> Ready on http://localhost:${process.env.PORT || 8080}`);
-    });
-  } catch (e) {
-    console.error("\u274C Critical Error:", e);
-    process.exit(1);
-  }
+      if (!req.headers["x-matched-path"]) {
+        req.headers["x-matched-path"] = pathname;
+      }
+      await requestHandler(req, res, parsedUrl);
+    } catch (err) {
+      console.error(err);
+      res.statusCode = 500;
+      res.end("Internal Error");
+    }
+  }).listen(parseInt(process.env.PORT || "8080"));
 }
 start();
